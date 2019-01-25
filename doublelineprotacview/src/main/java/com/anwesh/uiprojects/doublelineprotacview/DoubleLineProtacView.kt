e8bd95b80b2c887fe474c888f161e0e3819276e8
Anwesh43 <anweshthecool0@gmail.com>
2019-01-25 09:16:08 +0530

    adding libraries and defining constants

--- doublelineprotacview/src/main/java/com/anwesh/uiprojects/doublelineprotacview/DoubleLineProtacView.kt
+++ doublelineprotacview/src/main/java/com/anwesh/uiprojects/doublelineprotacview/DoubleLineProtacView.kt
@@ -4,3 +4,19 @@ package com.anwesh.uiprojects.doublelineprotacview
  * Created by anweshmishra on 25/01/19.
  */
 
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Canvas
+import android.graphics.Paint
+import android.graphics.Color
+import android.content.Context
+import android.app.Activity
+
+val nodes : Int = 5
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val sizeFactor : Float = 2.7f
+val strokeFactor : Int = 90
+val foreColor : Int = Color.parseColor("#673AB7")
+val backColor : Int = Color.parseColor("#BDBDBD")
